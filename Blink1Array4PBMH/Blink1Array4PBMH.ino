0cf153928955f55f2e555c7def3c9bed2615eedb
Mike Hewitt <matroxmike@icloud.com>
2015-08-02 22:19:02 +1000

    The LED display using an array to define number and order of LEDs

--- Blink1Array4PBMH/Blink1Array4PBMH.ino
+++ Blink1Array4PBMH/Blink1Array4PBMH.ino
@@ -11,6 +11,8 @@
  13-July-2015 fixed silly bug in loop() where 
  - lp was being incremented forever and delayvalue was being checked but never incremented/changed
  - so re-enabled the for loop and uploaded AND updated in git 
+ 14-July-2015 - prettier sequence
+ 20-July-2015 - fixed a modulus error added fancy loops
  */
 
 // Pin 13 has an LED connected on most Arduino boards. Not always RED but often found on the PCB
@@ -20,26 +22,42 @@
 //int led13 = 13;
 
 #define serialDebug 0
+#define debugDelayMult 1  // use 100
 #define delay1   50
+#include "/home/mike/lilypad/leddefinitions.h"
+//  https://www.arduino.cc/en/Hacking/BuildProcess
+
 
 // leds on lilypad USB are on pins 2,3,9,10,11,13  i.e. these are the output pins 
 // which of these can do PWM? 
 
-int ledarray [] {2,3,9,10,11,13}; // 13 is onboard  {2,3,9,10,11,13}
-const int numLEDS =  6; // 4 ;
 
 void ledFlash (int whichLED, int numTimes, int delayv) {
   for (int n=0;n<numTimes;n++){
+
+    if (serialDebug) {
+      Serial.print("whichLED=: ");
+      Serial.println(whichLED);
+    }
+
     digitalWrite(whichLED, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(delayv);
+    delay(delayv*debugDelayMult);
     digitalWrite(whichLED, LOW);   // turn the LED on (HIGH is the voltage level) 
     //   digitalWrite(led13, LOW);   // turn the LED on (HIGH is the voltage level)
 
-    delay(delayv);               // wait for a second
+    delay(delayv*debugDelayMult);               // wait for a second
   }
 }
-
-
+void allOn () {   // turn all LEDs on
+  for (int n=0;n<numLEDS;n++){
+    digitalWrite(ledarray[n], HIGH);   // turn the LED on (HIGH is the voltage level)
+  }
+}
+void allOff () {   // turn all LEDs on
+  for (int n=0;n<numLEDS;n++){
+    digitalWrite(ledarray[n], LOW);   // turn the LED on (HIGH is the voltage level)
+  }
+}
 void setup()  { 
   if (serialDebug) {
     Serial.begin(9600);
@@ -47,38 +65,89 @@ void setup()  {
       ; // wait for serial port to connect. Needed for Leonardo only
     }
   }
- // ledarray[0] = 9;
- // ledarray[1] = 10;
+  // ledarray[0] = 9;
+  // ledarray[1] = 10;
   //ledarray[2] = 2; 
- // ledarray[3] = 3; 
- // ledarray[4] = 13; 
+  // ledarray[3] = 3; 
+  // ledarray[4] = 13; 
   // declare pin 9 to be an output:
   for (int s=0; s<numLEDS; s++) {
     pinMode(ledarray[s], OUTPUT);
   }
 } 
 
-
-
 // the loop routine runs over and over again forever:
 void loop() {
+  Serial.print("loop() starting ");
   static int lp = 0; // 600;
   static int delayvalue  = 20;
-   for (int lp=0;lp< 6; lp++){
-  ledFlash (ledarray[lp % numLEDS], 4, delayvalue);
 
- // lp++;
-  if (delayvalue >= 600 )  {
-    delayvalue=0; 
+  for (int lp1=0;lp1<4; lp1++){
+
+    for (int lp=0;lp< numLEDS; lp++){
+      ledFlash (ledarray[lp], 4, delayvalue);
+      // lp++;
+      if (delayvalue >= 600 )  {
+        delayvalue=0; 
+      }
+    }
+
+    //  delay(1000); 
+    // allOn();    
+    // delay(1000);
+    // allOff ();
+    //delay(1000);
+
+
+    for (int lp=numLEDS;lp>=0; lp--){
+      ledFlash (ledarray[lp], 4, delayvalue);  // lp % numLEDS at ends of range
+      // lp++;
+      if (delayvalue >= 600 )  {
+        delayvalue=0; 
+      }
+    }
+  }
+  for (int lp=numLEDS;lp>0; lp--){
+    delay(1000); 
+    allOn();    
+    delay(1000);
+    allOff ();
   }
 
-    }
+  /*  for (int s=6;s>0; s--){
+   for (int lp=s;lp> 0; lp--){
+   ledFlash (ledarray[lp % numLEDS], 4, delayvalue);
+   if (delayvalue >= 600 )  {
+   delayvalue=0; 
+   }   
+   allOn();    
+   delay(1000);  
+   } 
+   }  
+   */
   //  delayvalue= delayvalue + 5;      // slow the blinking down over time
   if (serialDebug) {
     Serial.print("delayvalue=: ");
     Serial.println(delayvalue);
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ leddefinitions.h
@@ -0,0 +1,32 @@
+// leddefinitions.h
+// 14 July -2015
+//  #include "/home/mike/lilypad/leddefinitions.h"
+
+int ledarray [] = {13,2,3,9,10,11,13}; // 13 is onboard  {2,3,9,10,11,13}
+const int numLEDS =  7; // 4 ;
+//int ledarray [] = {2,3,4,5,6,7,8,9,10,11,12,13};
+// note about = above => arduino IDE enjoys with or without
+// gcc needs the = 
+// check what is really happening Mike
+
+//const int numLEDS =  12; // 4 ;
+// 19.07.2015 21:59:18
+
+// change ASAP for PB hat with 10 leds
+
+/*
+ * Arduino IDE uses this file to define the size and order of IO pins in an array
+ * the above values are for Lilypads. Below is a set of values for a standard UNO with a 
+ * 10 LED strip attached. Connected  pins are 6 through to 13 ( i.e. 8 pins)
+ * 20.07.2015 15:24:56
+ * name: unknown
+ * @param
+ * @return
+ * 
+ */
+//
+//int ledarray [] = {13,6,7,8,9,10,11,12,13};
+// const int numLEDS =  9; // 4 ;
+ // 20.07.2015 22:52:52
+ // if both ends are made to be the same then a loop can be created 
+ 
