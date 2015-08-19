6dcbe43de543fd3cb606ff686e930704eaa905f7
MatrixMike <matroxmike@icloud.com>
2015-08-19 16:17:12 +1000

    accelerometer1

--- ADXL3xx/ADXL3xx.ino
+++ ADXL3xx/ADXL3xx.ino
@@ -1,7 +1,7 @@
 
 /*
  ADXL3xx
- 
+
  Reads an Analog Devices ADXL3xx accelerometer and communicates the
  acceleration to the computer.  The pins used are designed to be easily
  compatible with the breakout boards from Sparkfun, available from:
@@ -16,36 +16,39 @@
  analog 3: x-axis
  analog 4: ground
  analog 5: vcc
- 
+
  created 2 Jul 2008
  by David A. Mellis
  modified 30 Aug 2011
- by Tom Igoe 
- 
+ by Tom Igoe
+
  This example code is in the public domain.
 
 */
+//  Mod Mike Hewitt 19-aug-2015
 
 // these constants describe the pins. They won't change:
 const int groundpin = 18;             // analog input pin 4 -- ground
 const int powerpin = 19;              // analog input pin 5 -- voltage
 const int xpin = A3;                  // x-axis of the accelerometer
-const int ypin = A2;                  // y-axis
-const int zpin = A1;                  // z-axis (only on 3-axis models)
+const int ypin = A4;// A2;            // y-axis
+const int zpin = A5;  //A1;           // z-axis (only on 3-axis models)
 
 void setup()
 {
   // initialize the serial communications:
   Serial.begin(9600);
-  
+
   // Provide ground and power by using the analog inputs as normal
   // digital pins.  This makes it possible to directly connect the
   // breakout board to the Arduino.  If you use the normal 5V and
   // GND pins on the Arduino, you can remove these lines.
+  /*
   pinMode(groundpin, OUTPUT);
   pinMode(powerpin, OUTPUT);
-  digitalWrite(groundpin, LOW); 
+  digitalWrite(groundpin, LOW);
   digitalWrite(powerpin, HIGH);
+  */
 }
 
 void loop()
