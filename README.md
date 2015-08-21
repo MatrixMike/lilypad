b19ca90dd403337acdf4bceb5c747e1c10b5a239
Mike Hewitt <matroxmike@icloud.com>
2015-08-21 13:01:23 +1000

    sync'd Arduino IDE version

--- README.md
+++ README.md
@@ -7,3 +7,16 @@ Modification on 13 July 2015
 really a git change test 
 see Blink1Array4PBMH.ino for details
 
+20.08.2015 13:11:37
+added PWM but strange effects when using it in conjunction with ON/OFF
+
+21.08.2015 10:55:14   Friday after Blackburn meeting
+
+integration of Arduino and Git: thoughts:
+git seems happy to work in one chosen directory with any subdirectories created
+
+Arduino wants to create it's own directory 
+e.g. to be tested : have an .ino file in git repo directory and open it in Arduino 
+	saving it seems to create  directory elsewhere 
+	
+
--- glove1/glove1/glove1.ino
+++ glove1/glove1/glove1.ino
@@ -7,9 +7,17 @@
    glove1 on 14-Aug-2015
    17-aug-2015
    19.08.2015 23:10:03      mod to add light sensor
+   21 August 2015
    */
 #define delaybase 6
 // explain what delaybase is for
+
+int fadeArray[] = {3, 9, 10, 11, 2, 13}; // array of all PWM capable LEDs
+int fadeArrayLen = 6;
+
+int led = 3;           // the pin that the LED is attached to
+int brightness = 0;    // how bright the LED is
+int fadeAmount = 5;    // how many points to fade the LED by
 
 // glove use pins 13,3,9,10,11
 int ledCount = 6;
@@ -25,7 +33,7 @@ const int xpin = A3;                  // x-axis of the accelerometer
 const int ypin = A4;// A2;            // y-axis
 const int zpin = A5;  //A1;           // z-axis (only on 3-axis models)
 #define ledDelay 100
-#define debugging 0         // a logical => true for serial o/p 
+#define debugging 1         // a logical => true for serial o/p 
 
 void setup() {  // maybe add a serial connection here to avoid the hangs
   // actually fixed the reloading refusal by double clicking on reset switch before uploading
@@ -36,9 +44,11 @@ void setup() {  // maybe add a serial connection here to avoid the hangs
     // initialize the serial communications:
     Serial.begin(9600);
   }
+  // pattern8();   PWM upsets the flow ATM
 }
 
 void loop() {
+  // You do not need to call pinMode() to set the pin as an output before calling analogWrite().
   // print the sensor values:
   LsensorValue = analogRead(lightPin);  // remove later
   XsensorValue = analogRead(xpin);      // remove later
@@ -50,28 +60,38 @@ void loop() {
     // print a tab between values:
     Serial.print("\t");
     Serial.print(YsensorValue);
-    // Serial.print(analogRead(ypin));
     Serial.print("\t");
     Serial.print(ZsensorValue);
-    //  Serial.print(analogRead(zpin));
     Serial.print("\t");
     Serial.print(LsensorValue);
-    //  Serial.print(analogRead(zpin));
     Serial.println();
-    // delay before next reading:
+
   }
   if (XsensorValue < 450) { // glove tips UP
     pattern6();
   }
-  if (YsensorValue < 450) { // thumb UP
-    pattern1a();
-  }
-  if (ZsensorValue < 450) { // palm UP
+  //*  if (YsensorValue > 600) { // thumb DOWN
+  //*    pattern8();
+  //*  }
+  if (YsensorValue < 450) { //
     pattern3();
   }
+  if (ZsensorValue > 400) { //
+    pattern1();
+  }
+  /*
+  if (ZsensorValue < 100) {
+    //  this is very common - now with glove on desk -
+    // something changed - faulty sensor - unlikely - or fluke early in testing
+    // avoid this
+    pattern1a();
+  }
+  */
+
   if (LsensorValue < 10) { // darkness
     pattern7();
   }
+  // delay before next reading:
   delay(100);
   //  pattern1a();
   //  pattern2();
@@ -84,21 +104,18 @@ void loop() {
 // need new patterns - like two middle fingers flashing    pins 3,9
 // need new patterns - like two outer  fingers flashing
 
-
-void pattern7() {   // middle two fingers
+#define LledDelay 25
+void pattern7() {   // two fingers
   for (int lp = 0; lp < delaybase; lp++) {
-    for (int thisLed = 3; thisLed < ledCount; thisLed++) {
+    for (int thisLed = 0; thisLed < 4; thisLed++) {
       digitalWrite(ledPins[thisLed], HIGH);
-      delay(ledDelay);
+      delay(LledDelay);
       digitalWrite(ledPins[thisLed], LOW);
+      delay(LledDelay);
     }
   }
 }
-
-
-
-
-void pattern6() {
+void pattern6() {   // two fingers
   for (int lp = 0; lp < delaybase; lp++) {
     for (int thisLed = 2; thisLed < 4; thisLed++) {
       digitalWrite(ledPins[thisLed], HIGH);
@@ -107,9 +124,6 @@ void pattern6() {
     }
   }
 }
-
-
-
 void pattern1() {
   for (int lp = 0; lp < delaybase; lp++) {
     for (int thisLed = 0; thisLed < ledCount; thisLed++) {
@@ -119,7 +133,7 @@ void pattern1() {
     }
   }
 }
-void pattern1a() {
+void pattern1a() {    // opposite direction to (1)
   for (int lp = 0; lp < delaybase; lp++) {
     for (int thisLed = 0; thisLed < ledCount; thisLed++) {
       digitalWrite(ledPins[ledCount - thisLed], HIGH);
@@ -137,20 +151,16 @@ void pattern2() {
     }
   }
 }
-
 void pattern3() {
   for (int lp = 0; lp < 10; lp++) {
     for (int thisLed = 0; thisLed < ledCount; thisLed++) {
       digitalWrite(ledPins[thisLed], HIGH);   // all high
     }
-
     delay(ledDelay * 2);                    // wait
-
     for (int thisLed = 0; thisLed < ledCount; thisLed++) {
       digitalWrite(ledPins[thisLed], LOW);    // now all low
     }
     delay(ledDelay * 2);                    // wait
-
   }
 }
 void pattern4() {
@@ -158,9 +168,7 @@ void pattern4() {
     for (int thisLed = 0; thisLed <= ledCount; thisLed = thisLed + 2) { // sort of alternate LED
       digitalWrite(ledPins[thisLed], HIGH);   // all high
     }
-
     delay(ledDelay * 2);                    // wait
-
     for (int thisLed = 0; thisLed < ledCount; thisLed = thisLed + 1) {
       digitalWrite(ledPins[thisLed], HIGH);   // all high
     }
@@ -168,18 +176,14 @@ void pattern4() {
       digitalWrite(ledPins[thisLed], LOW);    // now all low
     }
     delay(ledDelay * 2);                    // wait
-
   }
 }
-
 void pattern5() {
   for (int lp = 0; lp < 10; lp++) {
     for (int thisLed = 1; thisLed <= ledCount; thisLed = thisLed + 2) { // sort of alternate LED
       digitalWrite(ledPins[thisLed], HIGH);   // all high
     }
-
     delay(ledDelay * 2);                    // wait
-
     for (int thisLed = 0; thisLed < ledCount; thisLed = thisLed + 1) {
       digitalWrite(ledPins[thisLed], HIGH);   // all high
     }
@@ -187,6 +191,20 @@ void pattern5() {
       digitalWrite(ledPins[thisLed], LOW);    // now all low
     }
     delay(ledDelay * 2);                    // wait
-
   }
 }
+
+void pattern8() {
+  fadeAll();
+}
+void fadeAll() {
+  for (int f = 0; f < fadeArrayLen; f++) {
+    analogWrite(fadeArray[f], brightness);
+  }
+  brightness = brightness + fadeAmount;
+  if (brightness == 0 || brightness == 255) {
+    fadeAmount = -fadeAmount ;
+  }
+  delay(30);
+}
+
